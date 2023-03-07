72d6b5cd7b8dc37e5f29411deb445631af5b1c7d
jcm-art <87035357+jcm-art@users.noreply.github.com>
2023-03-07 08:07:00 -0800

    First commit for transcribvr

---
+++ Transcribvr.py
@@ -0,0 +1,13 @@
+
+
+class Transcibvr:
+    hello_text="Hello World"
+
+    def __init__(self) -> None:
+        pass
+
+    def hello_world(self):
+        print(self.hello_text) 
+
+my_transcibvr = Transcibvr()
+my_transcibvr.hello_world()
